3a6ce7bf19709377a86a29f0a1945d4526fdabe1
Enrique Berrios <enrique.berrios@student.tu-freiberg.de>
2022-06-21 12:25:08 +0200

    Grundstruktur Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Mobilephone 
+{
+    string ?PhoneNumber {get; set;}
+    bool PhoneState;
+    bool ConnectionState;
+    string ?OS {get; set;}
+
+    void receiveACall(string incomingNumber)
+    {
+
+    }
+    void receiveAMassage(string incomingNumber, string text)
+    {
+
+    }
+}
+class Smartphone : Mobilephone
+{
+    string ?Position {get; set;}
+    void ringAnAlarm()
+    {
+
+    }
+}
