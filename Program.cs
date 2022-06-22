efaf4d7796322404557bb140d460f5d94923d46c
Enrique Berrios <enrique.berrios@student.tu-freiberg.de>
2022-06-22 10:44:08 +0200

    Read SmartPhoneData.CSV and load data into Smartphone Instanz | deleted spacebar in SmartPhoneData.csv (Latitude)

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 {
     class Mobilephone 
     {
-        string ?PhoneNumber {get; set;}
-        bool PhoneState;
-        bool ConnectionState;
-        enum OS {A, B};
+        public string ?PhoneNumber {get; set;}
+        public string ?PhoneState;
+        public string ?ConnectionState;
+        public string ?OS;
         void makeACall(string number)
         {
 
@@ -25,7 +25,23 @@
     }
     class Smartphone : Mobilephone
     {
-        string ?Position {get; set;}
+        public string ?positionLatitude {get; set;}
+        public string ?positionLongitude {get; set;}
+        public Smartphone()
+        {
+
+        }
+        public Smartphone(string csvString)
+        {
+            string[] csvEntries = csvString.Split(',');
+
+            PhoneNumber = csvEntries[0];
+            OS = csvEntries[1];
+            PhoneState = csvEntries[2];
+            ConnectionState = csvEntries[3];
+            positionLatitude = csvEntries[4];
+            positionLongitude = csvEntries[5];
+        }
         void ringAnAlarm()
         {
             System.Console.WriteLine();
@@ -35,7 +51,23 @@
     {
         static void Main(string[] args)
         {
+            string pathSmartPhoneDataCsv = @"SmartPhoneData.csv";
+            Smartphone erstesSmartphone = null;
+            Smartphone zweitesSmartphone = null;
+            if(File.Exists(pathSmartPhoneDataCsv))
+            {
+                string[] smartphoneAsCsvString = File.ReadAllLines(pathSmartPhoneDataCsv);
 
+                erstesSmartphone = new Smartphone(smartphoneAsCsvString[1]);
+                zweitesSmartphone = new Smartphone(smartphoneAsCsvString[2]);
+            }
+            System.Console.WriteLine(erstesSmartphone.PhoneNumber);
+            System.Console.WriteLine(erstesSmartphone.OS);
+            System.Console.WriteLine(erstesSmartphone.PhoneState);            
+            System.Console.WriteLine(erstesSmartphone.ConnectionState);
+            System.Console.WriteLine(erstesSmartphone.positionLatitude);
+            System.Console.WriteLine(erstesSmartphone.positionLongitude);
+            System.Console.WriteLine(zweitesSmartphone);
         }
     }
 }
