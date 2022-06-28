6d0bfc26886b9fc54421897cce4a29c14af22595
Pbzdecry <pbz.daniel.krutsche@gmail.com>
2022-06-28 12:28:54 +0200

    master-branch

--- Program.cs
+++ Program.cs
@@ -28,44 +28,83 @@
             {
                 makeACall(ReceiverPhoneNumber);
             }
-            /*if (TestName == "Position")
+            if (TestName == "Position")
             {
-                ();
-            }*/
+                printPosition();
+            }
         }
         public override string ToString()
         {
             return TestNummer + " " + TestName + " " + PhoneNumber + " " + ReceiverPhoneNumber;
         }
     }
-    class Mobilephone 
+    public class Mobilephone
     {
-        public string ?PhoneNumber {get; set;}
-        public string ?PhoneState;
-        public string ?ConnectionState;
-        public string ?OS;
+        public string? PhoneNumber { get; set; }
+        public string PhoneState { get; set; }
+        public string ConnectionState { get; set; }
+        public string OS { get; set; }
+
+        public Mobilephone()
+        {
+            PhoneNumber = "110";
+            PhoneState = "silent";
+            ConnectionState = "offline";
+            OS = "OS_A";
+        }
+        public Mobilephone(string phone_number, string phone_state, string connection_state, string OS)
+        {
+            this.PhoneNumber = phone_number;
+            this.PhoneState = phone_state;
+            this.ConnectionState = connection_state;
+            this.OS = OS;
+        }
         public void makeACall(string number)
         {
-
+            if (ConnectionState == "online")
+            {
+                System.Console.WriteLine("{0} wird angerufen.", number);
+            }
+            else
+            {
+                System.Console.WriteLine("Verbindung unterbrochen. Es kann kein Sprachanruf durchgeführt werden.");
+            }
         }
         public void receiveACall(string incomingNumber)
         {
-            System.Console.WriteLine();
+            if (ConnectionState == "online" && PhoneState == "normal")
+            {
+                System.Console.WriteLine("Eingehender Anruf von {0}", incomingNumber);
+            }
+
         }
-        void sendAMessage(string number)
+        public void sendAMessage(/*string number, string text*/ Mobilephone phone, string text)
         {
+            if (phone.ConnectionState == "online" && phone.OS == "OS_A" && ConnectionState == "online")
+            {
+                Console.WriteLine("Nachricht wird an {0} gesendet.", phone.PhoneNumber);
+            }
+            else
+            {
+                Console.WriteLine("Die Nachricht kann nicht gesendet werden.\nMögliche Ursachen\n-unterbrochene Verbindung\n-Empfänger ist nicht online\nEmpfänger hat falsches Betriebssystem");
+            }
 
         }
         public void receiveAMessage(string incomingNumber, string text)
         {
-            System.Console.WriteLine();
+            if (ConnectionState == "online" && PhoneState == "normal" && OS == "OS_A")
+            {
+                Console.WriteLine("Neue Nachricht von {0}\n{1}", incomingNumber, text);
+            }
         }
     }
+    
+    
     class Smartphone : Mobilephone
     {
         public string ?positionLatitude {get; set;}
         public string ?positionLongitude {get; set;}
-        public Smartphone()
+        public Smartphone() 
         {
 
         }
@@ -82,7 +121,17 @@
         }
         public void ringAnAlarm()
         {
-            System.Console.WriteLine();
+            if (OS == "OS_B")
+            {
+                System.Console.WriteLine("ALarm! Alarm!");
+            }
+        }
+        public void printPosition()
+        {
+            if(positionLatitude != null && positionLongitude != null)
+            {
+                Console.WriteLine("Sie befinden sich an dieser Position: {0}, {1}", positionLatitude, positionLongitude);
+            }
         }
     }
     class Kommunikationskoordinator
